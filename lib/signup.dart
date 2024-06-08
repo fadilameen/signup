f3c59efbf1c5e0ef80e5c3d74d855bc11e3c8a62
fadilameen <fadilameen63@gmail.com>
2024-06-08 20:33:44 +0530

    08-06-24

--- lib/signup.dart
+++ lib/signup.dart
@@ -4,41 +4,9 @@ import 'package:flutter/material.dart';
 import 'textfield.dart';
 
 class signUp extends StatelessWidget {
-  signUp({super.key});
+  signUp({super.key, required this.mainContent});
 
-  final _formKey = GlobalKey<FormState>();
-
-  TextEditingController name = TextEditingController();
-  TextEditingController email = TextEditingController();
-  TextEditingController pass = TextEditingController();
-
-  String? _validatename(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Name cannot be empty';
-    }
-    return null;
-  }
-
-  String? _validateemail(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Email cannot be empty';
-    }
-    if (!value.contains('@')) {
-      return 'Not a valid email';
-    }
-    return null;
-  }
-
-  String? _validatepass(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Password cannot be empty';
-    }
-    if (value.length < 8) {
-      return 'Must be greater than 8 characters';
-    }
-    return null;
-  }
-
+  final Widget mainContent;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,62 +41,11 @@ class signUp extends StatelessWidget {
                   box(screenheight * 0.33, Color(0xFFB9DB7E)),
                   box(screenheight * 0.345, Color(0xFF3FB283)),
                   Positioned(
-                    left: 3,
-                    right: 3,
-                    top: screenheight * 0.36,
-                    bottom: 5,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(40),
-                        color: Color(0xFFFFFFFF),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(27, 40, 27, 40),
-                        child: Form(
-                          key: _formKey,
-                          child: ListView(
-                            children: [
-                              Center(
-                                child: Text(
-                                  "Create your account",
-                                  style: TextStyle(fontSize: 22),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 50,
-                              ),
-                              Txt(
-                                uptext: "Full Name",
-                                hinttext: "You Name",
-                                controller: name,
-                                validator: _validatename,
-                              ),
-                              Txt(
-                                uptext: "Email",
-                                hinttext: "Your email",
-                                controller: email,
-                                validator: _validateemail,
-                              ),
-                              Txt(
-                                uptext: "Password",
-                                hinttext: "Enter Password",
-                                controller: pass,
-                                validator: _validatepass,
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  if (_formKey.currentState!.validate()) {
-                                    print("validated successfully");
-                                  }
-                                },
-                                child: Text("Create Account"),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                      left: 3,
+                      right: 3,
+                      top: screenheight * 0.36,
+                      bottom: 5,
+                      child: mainContent),
                 ],
               );
             },
