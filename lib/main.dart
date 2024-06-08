b23574d3d5c060e52178b5e9968bb5a3c54269e7
fadilameen <fadilameen63@gmail.com>
2024-06-09 01:55:21 +0530

    9-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:signup/signup.dart';
 import 'package:signup/design1.dart';
@@ -45,28 +47,82 @@ class _MainAppState extends State<MainApp> {
                   width: 186,
                 ),
                 const Padding(
-                  padding: EdgeInsets.only(
-                    left: 30,
-                    right: 30,
-                  ),
+                  padding: EdgeInsets.only(left: 30, right: 30, bottom: 10),
                   child: Text(
                     textAlign: TextAlign.center,
                     'Never forget to go green everyday, because the greener you go the higher you get',
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const MyDesign(
-                          mainContent: SignUp(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 130,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xFF1DA684),
+                          Color(0xFFBEDC7C),
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(3),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MyDesign(
+                                mainContent: SignUp(),
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Sign Up",
+                          style: TextStyle(color: Colors.white, fontSize: 17),
                         ),
                       ),
-                    );
-                  },
-                  child: const Text("Sign Up"),
+                    ),
+                  ),
                 ),
+                Container(
+                  width: 130,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xFF1DA684),
+                        Color(0xFFBEDC7C),
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(3),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shadowColor: Colors.white,
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Log In",
+                        style: TextStyle(color: Colors.black, fontSize: 17),
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
           ),
